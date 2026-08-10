8d7aaa112672d25303fb1447b41f64a01b8a9d39
Sanjeev Singh <sksedu84@gmail.com>
2026-08-10 17:27:59 -0400

    added favicon

--- config/routes.py
+++ config/routes.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 api_router = APIRouter()
 
@@ -9,3 +12,12 @@ def routes(app: FastAPI) -> None:
 
     app.include_router(admin.router)
     app.include_router(health.router)
+
+    if not os.path.exists("static"):
+        os.makedirs("static")
+    
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+    @app.get("/favicon.ico", include_in_schema=False)
+    async def favicon():
+        return FileResponse("static/favicon.ico")
