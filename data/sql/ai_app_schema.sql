6011de1552c7c9eb8cb882639f509774e5e0b20e
Sanjeev Singh <sksedu84@gmail.com>
2026-08-16 07:58:04 -0400

    Database config setup

---
+++ data/sql/ai_app_schema.sql
@@ -0,0 +1,16 @@
+-- CREATING EXTENTIONS 
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- CLEAN UP
+DROP TABLE IF EXISTS langchain_pg_embedding;
+DROP TABLE IF EXISTS langchain_pg_collection;
+
+-- CREATING TABLES
+/*
+Auto created tables:
+	langchain_pg_collection
+	langchain_pg_embedding
+	
+*/
+
+-- CREATING INDEXS
